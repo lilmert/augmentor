4af630da32ecc798c32404ba20f305acf535bc99
Marty Whelan <marty.whelan@verafin.com>
2022-03-15 14:33:05 -0230

    updated to use cdragon assets and added lib for external calls

--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,35 @@
-import Head from "next/head";
+import { getAllChampions } from "../lib/champions";
 import Image from "next/image";
-import styles from "../styles/Home.module.css";
+import { Fragment } from "react/cjs/react.production.min";
 
-export default function Home() {
+const PORTRAIT_URL =
+  "https://raw.communitydragon.org/latest/plugins/rcp-be-lol-game-data/global/default/v1/champion-icons";
+
+export default function Home({ champs }) {
   return (
     <div>
-      <h1>Here</h1>
+      <ul>
+        {champs.map((champ) => (
+          <li key={champ.id}>
+            {champ.name}: {champ.roles}
+            <Image
+              src={PORTRAIT_URL + `/${champ.id}.png`}
+              alt={"Champ portrait"}
+              width={40}
+              height={40}
+            />
+          </li>
+        ))}
+      </ul>
     </div>
   );
+}
+
+export async function getStaticProps() {
+  const champs = await getAllChampions();
+  return {
+    props: {
+      champs,
+    },
+  };
 }
